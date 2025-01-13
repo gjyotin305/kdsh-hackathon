ed70d6775f4f762443efa7cc41d931635639773e
Jyotin Goel <b22ai063@iitj.ac.in>
2025-01-13 19:20:54 +0530

    API
    Signed-off-by: Jyotin Goel <b22ai063@iitj.ac.in>

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from loguru import logger
+from agent import router_conference
 from pydantic import BaseModel
 from fastapi import FastAPI
 
@@ -9,12 +10,18 @@ class APIRequest(BaseModel):
     pdf_path: str
 
 
+@app.post("/api/task2"):
+def recommender(body: APIRequest):
+    logger.info(f"TASK 2 | {body.pdf_path}")
+    
+    object = router_conference(
+        pdf_path=body.pdf_path
+    )
 
-
-
-
-
-
+    return {
+        "content": f"{object.conference_title}",
+        "metadata": f"{body.pdf_path}"
+    }
 
 
 @app.post("/api/task1")
